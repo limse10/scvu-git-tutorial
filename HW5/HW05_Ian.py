f0bf8693a8fa4dd08e949ca5f383f87051034763
Ian <ianvarella02@gmail.com>
2021-06-04 10:15:24 +0800

    added HW

---
+++ HW5/HW05_Ian.py
@@ -0,0 +1,161 @@
+#QN5
+'''def make_counter():
+    """Return a counter function.
+
+    >>> c = make_counter()
+    >>> c('a')
+    1
+    >>> c('a')
+    2
+    >>> c('b')
+    1
+    >>> c('a')
+    3
+    >>> c2 = make_counter()
+    >>> c2('b')
+    1
+    >>> c2('b')
+    2
+    >>> c('b') + c2('b')
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    dict = {}
+    def count(i):
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+        return dict[i]
+    return count
+'''
+
+#QN6
+'''def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    """
+    "*** YOUR CODE HERE ***"
+    # 0 1 1 2 3 5 8 13
+    a, b = 0, 1
+    def fib_count():
+        nonlocal a
+        total = a
+        nonlocal b
+        a, b = b, a + b
+        return total
+    return fib_count'''
+
+#Q7
+'''def make_withdraw(balance, password):
+    """Return a password-protected withdraw function.
+
+    >>> w = make_withdraw(100, 'hax0r')
+    >>> w(25, 'hax0r')
+    75
+    >>> error = w(90, 'hax0r')
+    >>> error
+    'Insufficient funds'
+    >>> error = w(25, 'hwat')
+    >>> error
+    'Incorrect password'
+    >>> new_bal = w(25, 'hax0r')
+    >>> new_bal
+    50
+    >>> w(75, 'a')
+    'Incorrect password'
+    >>> w(10, 'hax0r')
+    40
+    >>> w(20, 'n00b')
+    'Incorrect password'
+    >>> w(10, 'hax0r')
+    "Your account is locked. Attempts: ['hwat', 'a', 'n00b']"
+    >>> w(10, 'l33t')
+    "Your account is locked. Attempts: ['hwat', 'a', 'n00b']"
+    >>> type(w(10, 'l33t')) == str
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    wrong_attempts = []
+    no_of_attempts = 0
+    def withdraw(amount, password_attempt):
+        nonlocal no_of_attempts
+        if password_attempt == password and no_of_attempts < 3:
+            nonlocal balance
+            if amount > balance:
+                return 'Insufficient funds'
+            balance = balance - amount
+            return balance
+        elif password_attempt != password and no_of_attempts < 3:
+            wrong_attempts.append(password_attempt)
+            no_of_attempts += 1
+            return 'Incorrect password'
+        if no_of_attempts >= 3:
+            return "Your account is locked. Attempts: " + str(wrong_attempts)
+            
+    return withdraw'''
+
+#QN8
+'''def make_joint(withdraw, old_password, new_password):
+    """Return a password-protected withdraw function that has joint access to
+    the balance of withdraw.
+
+    >>> w = make_withdraw(100, 'hax0r')
+    >>> w(25, 'hax0r')
+    75
+    >>> make_joint(w, 'my', 'secret')
+    'Incorrect password'
+    >>> j = make_joint(w, 'hax0r', 'secret')
+    >>> w(25, 'secret')
+    'Incorrect password'
+    >>> j(25, 'secret')
+    50
+    >>> j(25, 'hax0r')
+    25
+    >>> j(100, 'secret')
+    'Insufficient funds'
+
+    >>> j2 = make_joint(j, 'secret', 'code')
+    >>> j2(5, 'code')
+    20
+    >>> j2(5, 'secret')
+    15
+    >>> j2(5, 'hax0r')
+    10
+
+    >>> j2(25, 'password')
+    'Incorrect password'
+    >>> j2(5, 'secret')
+    "Your account is locked. Attempts: ['my', 'secret', 'password']"
+    >>> j(5, 'secret')
+    "Your account is locked. Attempts: ['my', 'secret', 'password']"
+    >>> w(5, 'hax0r')
+    "Your account is locked. Attempts: ['my', 'secret', 'password']"
+    >>> make_joint(w, 'hax0r', 'hello')
+    "Your account is locked. Attempts: ['my', 'secret', 'password']"
+    """
+    "*** YOUR CODE HERE ***"
+    passwordcheck = withdraw(0, old_password)
+    if type(passwordcheck) == str:
+        return passwordcheck
+    def jointacct(amount, password):
+        if password == new_password:
+            return withdraw(amount, old_password)
+        else:
+            return withdraw(amount, password)
+    return jointacct'''
